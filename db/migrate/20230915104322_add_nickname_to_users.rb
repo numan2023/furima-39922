28055e8b3f5edb4f5675ca310ecfdf1361658b0a
numan2023 <fullmetal0212@gmail.com>
2023-09-17 10:27:30 +0900

    ユーザー管理機能

---
+++ db/migrate/20230915104322_add_nickname_to_users.rb
@@ -0,0 +1,10 @@
+class AddNicknameToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :nickname, :string
+    add_column :users, :sei,      :string
+    add_column :users, :mei,      :string
+    add_column :users, :sei_kana, :string
+    add_column :users, :mei_kana, :string
+    add_column :users, :birthday, :date
+  end
+end
