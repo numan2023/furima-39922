3d11ab9b8a333445580692a4050d51d7a1ab4baf
numan2023 <fullmetal0212@gmail.com>
2023-09-17 11:01:58 +0900

    ユーザー管理機能(robocop実行済)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -79,7 +79,7 @@ RSpec.describe User, type: :model do
       another_user.valid?
       expect(another_user.errors.full_messages).to include('Email has already been taken')
     end
-   it 'emailは@を含まないと登録できない' do
+    it 'emailは@を含まないと登録できない' do
       @user.email = 'testmail'
       @user.valid?
       expect(@user.errors.full_messages).to include('Email is invalid')
