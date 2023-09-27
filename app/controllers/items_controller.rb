496f28792dd6f188176ec4691a0be672f02dc2d6
numan2023 <fullmetal0212@gmail.com>
2023-09-27 20:18:57 +0900

    商品情報編集機能#8 レビュー修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_tweet, only: [:edit, :show, :update]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :set_tweet, only: [:show, :edit, :update, :move_to_index]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.order('created_at DESC')
@@ -46,8 +46,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
-    unless current_user.id == @item.user_id
+    unless user_signed_in? && current_user.id == @item.user_id
       redirect_to root_path
     end
   end
